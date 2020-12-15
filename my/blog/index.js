e5a2e84b37e4c8bd188f43f6f8a5a598a8f3109c
bitbloxhub <jonahkamp@gmail.com>
2020-12-15 10:21:38 -0800

    made js look nice and inline SVG

--- my/blog/index.js
+++ my/blog/index.js
@@ -1,31 +1,32 @@
 function getListing() {
-  var postList = JSON.parse(geturl("/_data/blog/postlist.json"));
-  postList.reverse();
-  var returnHtml = "";
-  var postsRendered=[];
-  for (const post of postList){
-    var postElement="";
-    var metadata=JSON.parse(geturl(`/_data/blog/posts/${post}/meta.json`));
-    var postImagePath;
-    if (metadata["has-photo"]) {
-      postImagePath=`/_data/blog/posts/${post}/image.svg`;
-    } else {
-      postImagePath=`/_data/blog/noimage.svg`;
-    };
-    var posturl=new URL("/my/blog/readpost.html",location.origin);
-    posturl.searchParams.set("postname",post);
-    var postElementInside=`
+	var postList = JSON.parse(geturl("/_data/blog/postlist.json"));
+	postList.reverse();
+	var returnHtml = "";
+	var postsRendered = [];
+	for (const post of postList) {
+		var postElement = "";
+		var metadata = JSON.parse(geturl(`/_data/blog/posts/${post}/meta.json`));
+		var postImagePath;
+		if (metadata["has-photo"]) {
+			postImagePath = `/_data/blog/posts/${post}/image.svg`;
+		} else {
+			postImagePath = `/_data/blog/noimage.svg`;
+		};
+		var postImageSvg = geturl(postImagePath);
+		var posturl = new URL("/my/blog/readpost.html", location.origin);
+		posturl.searchParams.set("postname", post);
+		var postElementInside = `
     <a href="${posturl.href}"><h2>${metadata["title"]}</h2></a>
     <p>published ${metadata["published-human"]}</p>
-    <img src="${postImagePath}" height="480" width="640" />
+    ${postImageSvg}
     `;
-    postElement=`<li>${postElementInside}</li>`
-    postsRendered.push(postElement)
-  };
-  var returnHtmlList=postsRendered.join("");
-  returnHtml=`<ul class="extra_list_nolist" style="text-align:center">${returnHtmlList}</ul>`;
-  return returnHtml;
+		postElement = `<li>${postElementInside}</li>`
+		postsRendered.push(postElement)
+	};
+	var returnHtmlList = postsRendered.join("");
+	returnHtml = `<ul class="extra_list_nolist" style="text-align:center">${returnHtmlList}</ul>`;
+	return returnHtml;
 };
 
-var blogDiv=document.querySelector("#blog_div");
-blogDiv.innerHTML=getListing();
+var blogDiv = document.querySelector("#blog_div");
+blogDiv.innerHTML = getListing();
--- my/utils.js
+++ my/utils.js
@@ -1,6 +1,10 @@
 function geturl(theUrl) {
-  var resptext = jQuery.get({url:theUrl,async:false}).responseText;
-  console.log("AJAX response");
-  console.log(resptext);
-  return resptext;
+	var resptext = jQuery.get({
+			url: theUrl
+			, async: false
+		})
+		.responseText;
+	console.log("AJAX response");
+	console.log(resptext);
+	return resptext;
 };
