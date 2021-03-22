12a0075287a368d46b202c144fb0d66898a441f4
bitbloxhub <jonahkamp@gmail.com>
2021-03-22 09:52:28 -0700

    update albums

--- my/albums/getalbum.js
+++ my/albums/getalbum.js
@@ -1,12 +1,28 @@
 function albumDownload(albumname) {
   var html="";
   albumMeta=JSON.parse(geturl(`/_data/albums/${albumname}/albuminfo.json`));
-  html=`
-    <h1>${albumMeta["name"]}</h1>
-    <p> in format ${albumMeta["format"]}</p>
-    <p> md5sum:${albumMeta["md5sum"]} </p>
-    <a href="/_data/albums/${albumname}/album.${albumMeta["extension"]}" download>download now!</a>
-  `;
+  if (!albumMeta.hasOwnProperty("version")) {
+    html=`
+      <h1>${albumMeta["name"]}</h1>
+      <p> in format ${albumMeta["format"]}</p>
+      <p> md5sum:${albumMeta["md5sum"]} </p>
+      <a href="/_data/albums/${albumname}/album.${albumMeta["extension"]}" download>download now!</a>
+    `;
+  };
+  if (albumMeta["version"]==2) {
+    cover = geturl(albumMeta["cover"]);
+    html+=`
+      <h1>${albumMeta["name"]}</h1>
+      ${cover}
+    `;
+    var dloptions=albumMeta["dloptions"];
+    html+=`
+      <h2>Downloads</h2>
+    `;
+    dloptions.forEach((dloption, i) => {
+      html+=`<a href="${dloption["url"]}>${dloption["info"]}</a>"`;
+    });
+  };
   return html;
 };
 
