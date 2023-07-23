2097ec16e86f506b9f8e179ae96413d6183f7bc5
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2023-07-23 14:34:15 -0700

    use mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from "astro/config"
 
 import svelte from "@astrojs/svelte"
 import UnoCSS from "unocss/astro"
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://bitbloxhub.github.io",
 	integrations: [
 		svelte(),
-		UnoCSS()
+		UnoCSS(),
+		mdx()
 	],
 	experimental: {
 		viewTransitions: true
